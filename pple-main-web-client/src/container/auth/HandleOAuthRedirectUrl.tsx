c6b799ef2f05f5a82fab18f2d5c7c11fe951f594
wook <gus5427@naver.com>
2022-02-22 23:27:16 +0900

    Fix : fix not show displayName

--- pple-main-web-client/src/container/auth/HandleOAuthRedirectUrl.tsx
+++ pple-main-web-client/src/container/auth/HandleOAuthRedirectUrl.tsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { getKakaoRedirectURL } from '../../api/oatuh';
-import { customAxios } from '../../lib/customAxios';
 
 const HandleOAuthRedirectUrl = () => {
   const [searchParams, setSearchParams] = useSearchParams();
