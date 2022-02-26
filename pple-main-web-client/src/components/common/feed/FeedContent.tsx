e43344533483d40b82e78fd08d437263dc91dec2
wook <gus5427@naver.com>
2022-02-27 01:24:51 +0900

    Design : modify 제목, 내용 사이 간격

--- pple-main-web-client/src/components/common/feed/FeedContent.tsx
+++ pple-main-web-client/src/components/common/feed/FeedContent.tsx
@@ -9,7 +9,7 @@ const FeedContentBlock = styled('div')({
     lineHeight: '22px',
     letterSpacing: '-0.03em',
     color: '#222222',
-    margin: '15px 0px',
+    margin: '10px 0px',
   },
   '& .content': {
     fontStyle: 'normal',
@@ -18,7 +18,7 @@ const FeedContentBlock = styled('div')({
     lineHeight: '20px',
     letterSpacing: '-0.03em',
     color: '#222222',
-    marginBottom: '15px',
+    marginBottom: '10px',
   },
 });
 
@@ -30,17 +30,9 @@ interface Props {
 const FeedContent: React.FC<Props> = ({ title, content }) => {
   return (
     <FeedContentBlock>
-      <div className="title">
-        {title ? title : '아버지가 재생불량성빈혈을 판정을 받았습니다'}
-      </div>
+      <div className="title">{title ? title : '내용이 없습니다'}</div>
       <div className="content">
-        {content ? (
-          <pre>{content}</pre>
-        ) : (
-          `아빠가 11월 2일 급성백혈병 재발로 인해 이식을 받으시고 회복중에 장출혈이
-        생기면서 매일 혈소판 수혈을 받으시고 계신 상황입니다. <br/>병원에서
-        지정헌혈을 요청하셔서 백방으로 알아보는데 힘든상황입니다."`
-        )}
+        {content ? <pre>{content}</pre> : `내용이 없습니다.`}
       </div>
     </FeedContentBlock>
   );
