7f93abb88a441fbe868afe697e0c4516308bf84a
wook <gus5427@naver.com>
2022-02-28 19:42:09 +0900

    Chore : add 소개페이지 이미지

--- pple-main-web-client/src/pages/Introduce.tsx
+++ pple-main-web-client/src/pages/Introduce.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import intro1 from '../static/images/intro/introPage1.png';
+import intro2 from '../static/images/intro/introPage2.png';
 const Introduce = () => {
-  return <div></div>;
+  const imageStyle = {
+    background: 'white',
+    width: '100%',
+  };
+  return (
+    <div>
+      <div>
+        <img src={intro1} alt="" style={imageStyle} />
+      </div>
+      <div>
+        <img src={intro2} alt="" style={imageStyle} />
+      </div>
+    </div>
+  );
 };
 
 export default Introduce;
