5199961ce0b47c25fa9bbd538b847d63a26f7488
wook <gus5427@naver.com>
2022-03-13 16:38:24 +0900

    Feat : add 댓글 Redux

--- pple-main-web-client/src/models/index.ts
+++ pple-main-web-client/src/models/index.ts
@@ -1,20 +1,22 @@
-import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 // session Storage 에 저장
-import storage from 'redux-persist/lib/storage'; 
+import storage from 'redux-persist/lib/storage';
 
-import account from "./auth/account";
-
+import account from './auth/account';
+import search from './search/search';
+import comment from './comment';
 const persistConfig = {
-    key: "root", 
-    storage, 
-    whileList : ["account"],
+  key: 'root',
+  storage,
+  whileList: ['account'],
 };
 
 const rootReducer = combineReducers({
-    account
+  account,
+  search,
+  comment,
 });
 
-export type RootState = ReturnType<typeof rootReducer>; 
-export default persistReducer(persistConfig,rootReducer); 
-	
+export type RootState = ReturnType<typeof rootReducer>;
+export default persistReducer(persistConfig, rootReducer);
