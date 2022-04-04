a236ac2b4a780a2f314ffeb0c8ac24796e48d797
wook <gus5427@naver.com>
2022-04-04 20:10:08 +0900

    Feat : 500 error 메세지 문 출력

--- pple-main-web-client/src/lib/util/error.ts
+++ pple-main-web-client/src/lib/util/error.ts
@@ -1,11 +1,14 @@
 export const notifyError = (errorCode: number) => {
   switch (errorCode) {
     case 404:
+    case 500:
       alert('서버 문제입니다. 관리자에게 문의해주세요 ');
       break;
+
     case 400:
       alert('Bad Request');
       break;
+
     default:
       break;
   }
