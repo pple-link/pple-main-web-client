1be7fd59a293e10df76677d0f0f7b3ede04a8821
wook <gus5427@naver.com>
2022-05-03 03:09:55 +0900

    ff

--- pple-main-web-client/src/container/feed/DetailForm.tsx
+++ pple-main-web-client/src/container/feed/DetailForm.tsx
@@ -54,13 +54,15 @@ const DetailForm: React.FC = () => {
     }
   };
 
-  async function getResponse(){
-    const response = await fetch(`http://ppledevtest-env.eba-9fa279up.ap-northeast-2.elasticbeanstalk.com/api/v1/donation/detail/${donationUuid}`,{
+  function getResponse(){
+    fetch(`http://ppledevtest-env.eba-9fa279up.ap-northeast-2.elasticbeanstalk.com/api/v1/donation/detail/${donationUuid}`,{
       method: 'GET',
       mode: "no-cors"
-    });
-    const data = await response.json();
-    console.log(data.data);
+    }).then(response =>
+        console.log(response.json()))
+        .catch(err=>{
+          console.log(err);
+        });
   }
 
   useEffect( () => {
