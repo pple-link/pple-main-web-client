54458aa8dfdfba2404cc9484884a2150f130bf90
wook <gus5427@naver.com>
2022-05-03 01:57:09 +0900

    aaa

--- pple-main-web-client/src/lib/api/donation.test.ts
+++ pple-main-web-client/src/lib/api/donation.test.ts
@@ -1,6 +1,5 @@
-export const getOneDonationByEncodedParameter = (encoded: string) =>{
-    return fetch(`http://ppledevtest-env.eba-9fa279up.ap-northeast-2.elasticbeanstalk.com/api/v1/donation/detail/${encoded}`,{
-        mode: 'cors',
-        method: 'GET',
-    });
+import {customAxios} from "../customAxios";
+
+export const getOneDonationByEncodedParameter = (encoded: string): Promise<void> => {
+    return customAxios.get(`/api/v1/donation/detail/${encoded}`);
 }
