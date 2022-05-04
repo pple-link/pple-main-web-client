afb9d6c01f9ff5b934f8b3433fc7466f0319ce94
wook <gus5427@naver.com>
2022-05-04 13:41:58 +0900

    TEST : add test function of getting url for share

--- pple-main-web-client/src/lib/api/donation.test.ts
+++ pple-main-web-client/src/lib/api/donation.test.ts
@@ -3,3 +3,20 @@ import {customAxios} from "../customAxios";
 export const getOneDonationByEncodedParameter = (encoded: string) => {
     return customAxios(`/api/v1/donation/detail/${encoded}`);
 }
+
+export const getShareUrl = (donationUuid: string, accessToken: string): string=>{
+    const url = "";
+    const body = {
+        donationUuid : donationUuid,
+    }
+    customAxios.post('/api/v1/donation/url/test',body,{
+     headers:{
+         'X-AUTH-TOKEN': `${accessToken}`,
+     }
+    }).then(res=>{
+        console.log(res);
+    }).catch(err=>{
+        console.log(err);
+    })
+    return url;
+}
